b85f37221c15a9b2b1936a1af8fa4c4a4e816609
Nichita <kabanesky1207@gmail.com>
2025-03-16 17:24:39 +0200

    contact.h

--- contact.h
+++ contact.h
@@ -1,6 +1,5 @@
 #ifndef CONTACT_H
 #define CONTACT_H
-
 #include <iostream>
 #include <string>
 
@@ -9,18 +8,10 @@ private:
     std::string phone;
     std::string city;
     std::string country;
-
 public:
-    
-    Contact();
-
-    
-    Contact(const std::string& phone);
-    Contact(const std::string& phone, const std::string& city);
     Contact(const std::string& phone, const std::string& city, const std::string& country);
-
-   
+    Contact();
     void Show() const;
 };
 
-#endif
+#endif // CONTACT_H
