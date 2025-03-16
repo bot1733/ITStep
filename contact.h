3dadf61c4d0e7c4fce524d5f819d99ff988100e9
Nichita <kabanesky1207@gmail.com>
2025-03-16 17:28:55 +0200

    contact.h

--- contact.h
+++ contact.h
@@ -1,15 +1,1 @@
-#ifndef COLLAGE_H
-#define COLLAGE_H
-#include "Contact.h"
 
-class Collage {
-private:
-    Contact contact;
-    std::string name;
-public:
-    Collage(const std::string& name, const Contact& contact);
-    Collage();
-    void Show() const;
-};
-
-#endif // COLLAGE_H
