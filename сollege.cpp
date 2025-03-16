b5efffafd0de7416a0e678b92e60666a18b2ffab
Nichita <kabanesky1207@gmail.com>
2025-03-16 17:36:07 +0200

    сollege.cpp

--- сollege.cpp
+++ сollege.cpp
@@ -1,1 +1,12 @@
+#include "College.h"
+
+College::College(const std::string& name, Contact* contacts) : name(name), contacts(contacts) {}
+
+void College::Show() const {
+    std::cout << "College: " << name << std::endl;
+    contacts->Show();
+}
+College::~College() {
+    delete contacts;
+}
 
