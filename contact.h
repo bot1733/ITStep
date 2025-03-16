bb8a20a40658266b22da656b714b87e580a685c7
Nichita <kabanesky1207@gmail.com>
2025-03-16 17:32:27 +0200

    contact.h

--- contact.h
+++ contact.h
@@ -1,1 +1,26 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <string>
+#include <iostream>
+
+class Contact {
+private:
+    std::string phone;
+    std::string city;
+    std::string country;
+
+public:
+    
+    Contact();
+    
+    Contact(const std::string& phone, const std::string& city, const std::string& country);
+    
+    void Show() const;
+    
+   
+    ~Contact();
+};
+
+#endif
 
